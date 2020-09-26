43ee026639c467e5c954b739a6ca4c62ff965dd0
FrederikJatzkowski <69867759+FrederikJatzkowski@users.noreply.github.com>
2020-09-26 17:53:03 +0200

    Test

---
+++ django_app/hostRouting/views.py
@@ -0,0 +1,42 @@
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+
+    path = request.path.split('/')
+
+    #Leere Elemente entfernen
+    while "" in path:
+        path.remove("")
+
+    #Letztes Element
+    if len(path) > 0:
+        last = path[len(path)-1]
+    else:
+        last = "false"
+
+    try:
+
+        f = open(str("static/angular-present/" + last))
+
+        if last[-2:] == 'js':
+            return HttpResponse(f,'text/javascript')
+
+        elif last[-3:] == 'css':
+            return HttpResponse(f,'text/css')
+
+        elif last[-3:] == 'ico':
+            return HttpResponse(f,'image/x-icon')
+
+        else:
+            return HttpResponse(f,'text/html')
+    
+    except:
+
+        f = open("static/angular-present/index.html")
+
+        return HttpResponse(f)
+
+
+
+    
